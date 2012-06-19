3de105ce655bd020d2d9379a8b800b12576968d4
Dean Dieker <ddieker@gmail.com>
2012-06-19 15:04:02 -0400

    first commit lol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheChang::Application.config.secret_token = 'acbb080f7a54baa226b7303c8737c237b6812dff3f6b7a3330bc22ec8894aeb942e811ef6f80d05816878b2448dc64cf4190dca8f7d011d4ab780c21e8e1640b'
